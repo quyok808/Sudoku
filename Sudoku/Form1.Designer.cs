74f575a6edb3c1080bc82b8fc9d1cadf3ec674e5
CongQuy252 <nva2572003@gmail.com>
2023-09-17 01:04:56 +0700

    done

---
+++ Sudoku/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Create_Matrix = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_check = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.Solve = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(592, 485);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " ";
+            // 
+            // Create_Matrix
+            // 
+            this.Create_Matrix.BackColor = System.Drawing.Color.LightSalmon;
+            this.Create_Matrix.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Create_Matrix.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Create_Matrix.Location = new System.Drawing.Point(613, 454);
+            this.Create_Matrix.Name = "Create_Matrix";
+            this.Create_Matrix.Size = new System.Drawing.Size(247, 45);
+            this.Create_Matrix.TabIndex = 1;
+            this.Create_Matrix.Text = "Create";
+            this.Create_Matrix.UseVisualStyleBackColor = false;
+            this.Create_Matrix.Click += new System.EventHandler(this.Create_Matrix_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sudoku.Properties.Resources._318yFRzGqVL;
+            this.pictureBox1.Location = new System.Drawing.Point(613, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 219);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_check
+            // 
+            this.button_check.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button_check.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_check.Enabled = false;
+            this.button_check.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_check.Location = new System.Drawing.Point(613, 401);
+            this.button_check.Name = "button_check";
+            this.button_check.Size = new System.Drawing.Size(247, 45);
+            this.button_check.TabIndex = 5;
+            this.button_check.Text = "Check";
+            this.button_check.UseVisualStyleBackColor = false;
+            this.button_check.Click += new System.EventHandler(this.button_check_Click);
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button_exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_exit.Location = new System.Drawing.Point(613, 301);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(120, 41);
+            this.button_exit.TabIndex = 6;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // Solve
+            // 
+            this.Solve.BackColor = System.Drawing.Color.Violet;
+            this.Solve.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Solve.Enabled = false;
+            this.Solve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Solve.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Solve.ForeColor = System.Drawing.Color.Black;
+            this.Solve.Location = new System.Drawing.Point(740, 301);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(120, 41);
+            this.Solve.TabIndex = 6;
+            this.Solve.Text = "Solve";
+            this.Solve.UseVisualStyleBackColor = false;
+            this.Solve.Click += new System.EventHandler(this.button_Solve);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reset.Enabled = false;
+            this.Reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Reset.Location = new System.Drawing.Point(613, 348);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(247, 45);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.button_Reset);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(613, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 35);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "5:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(866, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Solve);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.button_check);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Create_Matrix);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sudoku";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Create_Matrix;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_check;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Sudoku/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int n = 3;
+        public string[,] hide = new string[n * n, n * n];
+        public int[,] solve = new int[n * n, n * n];
+        public int[,] map = new int[n*n,n*n];
+        public Button[,] btns = new Button[n*n,n*n];
+
+        #region button event
+        private void Create_Matrix_Click(object sender, EventArgs e)
+        {
+            GenerateMap();
+            Button create = (Button)sender;
+            create.Enabled = false;
+            button_check.Enabled = true;
+            Reset.Enabled = true;
+            Solve.Enabled = true;
+            timer1.Start();
+        }
+        int gtri;
+        private void O1_Click(object sender, MouseEventArgs e)
+        {
+            
+            Button btn = sender as Button;
+            if (MouseButtons.Left == e.Button)
+            {
+                if (gtri == 9)
+                {
+                    gtri = 0;
+                }
+                gtri++;
+            }
+            else if (MouseButtons.Right == e.Button)
+            {
+                if (gtri == 1)
+                {
+                    gtri = 10;
+                }
+                gtri--;
+            }
+            btn.ForeColor = Color.Black;
+            btn.Text = gtri.ToString();
+        }
+        private void button_check_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            for (int i =0; i < n*n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    var btnText = btns[i, j].Text;
+                    if (btnText != map[i, j].ToString())
+                    {
+                        flag = 1;
+                        btns[i,j].ForeColor = Color.Red;
+                    }else
+                    {
+                        btns[i, j].ForeColor = Color.Blue;
+                        
+                    }  
+                }
+            }
+            if (flag == 0)
+                MessageBox.Show("Dung roi. Chuc mung ban!!!");
+            
+        }
+        private void button_exit_Click(object sender, EventArgs e) => Close();
+        private void button_Solve(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    if (btns[i, j].Text.Equals(hide[i, j]))
+                        btns[i, j].BackColor = Color.Gray;
+                    else if (btns[i,j].Text == map[i, j].ToString())
+                    {
+                        btns[i, j].BackColor = Color.Yellow;
+                    }
+                    
+                }
+            }
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    btns[i, j].Text = solve[i, j].ToString();
+                    btns[i, j].Enabled = false;
+                }
+            }
+            timer1.Stop();
+            Solve.Enabled = false;
+            button_check.Enabled = false;
+        }
+
+        private void button_Reset(object sender, EventArgs e)
+        {
+            GenerateMap();
+            button_check.Enabled = true;
+            Solve.Enabled = true;
+            value_timer = 300;
+            label1.Text = "5:00";
+            timer1.Start();
+        }
+        
+        #endregion
+        #region Tao bang sudoku
+        public void GenerateMap()
+        {
+            for (int i = 0;i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    map[i,j] = (i*n+i/n+j) % (n*n)+1;
+                    btns[i, j] = new Button();
+                }
+            }
+            Random rd = new Random();
+            for (int i = 0; i < 40; i++)
+            {
+                SuffleMap(rd.Next(0, 5));
+            }
+            CreateMap();
+            HideCell();
+        }
+        public void CreateMap()
+        {
+            groupBox1.Controls.Clear();
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    Button btn = new System.Windows.Forms.Button();
+                    btns[i, j] =btn;
+                    btn.BackColor = System.Drawing.Color.White;
+                    btn.FlatStyle = FlatStyle.Popup;
+
+                    btn.Name = (j * 9 + i + 1).ToString();
+                    btn.Size = new System.Drawing.Size(39, 34);
+                    btn.TabIndex = 0;
+                    btn.Text = map[i,j].ToString();
+
+                    btn.UseVisualStyleBackColor = false;
+                    btn.MouseUp += O1_Click;
+
+                    btn.Location = new System.Drawing.Point(17 + i * 47, 22 + j * 40);
+                    groupBox1.Controls.Add(btn);
+                    solve[i, j] = map[i, j];
+                }
+            }
+        }
+        public void SuffleMap(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    MatrixTransposition();
+                    break;
+                case 1:
+                    SwapRowInBlock();
+                    break;
+                case 2:
+                    SwapColInBlock();
+                    break;
+                case 3:
+                    SwapBlocksInRow();
+                    break;
+                case 4:
+                    SwapBlocksInCol();
+                    break;
+                default:
+                    MatrixTransposition();
+                    break;
+
+            }
+        }
+        #region Doi cho lm cho cac o khong trung gia tri
+        public void MatrixTransposition()
+        {
+            int[,] tmap = new int[n * n, n * n];
+            for (int i = 0; i < n*n; i++) 
+            {
+                for (int j = 0; j < n*n; j++)
+                {
+                    tmap[i,j] = map[j,i];
+                }
+            }
+            map = tmap;
+        }
+        public void SwapRowInBlock()
+        {
+            Random rd = new Random();
+            var Block = rd.Next(0, n);
+            var row1 = rd.Next(0,n);
+            var line1 = Block * n + row1;
+            var row2 = rd.Next(0, n);
+            while (row1 == row2)
+            {
+                row2 = rd.Next(0, n);
+            }
+            var line2 = Block * n + row2;
+            for (int i = 0; i < n*n;i++)
+            {
+                var temp = map[line1, i];
+                map[line1,i] = map[line2,i];
+                map[line2,i] = temp;
+            }
+        }
+
+        public void SwapColInBlock()
+        {
+            Random rd = new Random();
+            var Block = rd.Next(0, n);
+            var row1 = rd.Next(0, n);
+            var line1 = Block * n + row1;
+            var row2 = rd.Next(0, n);
+            while (row1 == row2)
+            {
+                row2 = rd.Next(0, n);
+            }
+            var line2 = Block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                var temp = map[i, line1];
+                map[ i, line1] = map[i,line2];
+                map[i,line2] = temp;
+            }
+        }
+
+        public void SwapBlocksInRow()
+        {
+            Random rd = new Random();
+            var Block1 = rd.Next(0, n);
+            var Block2 = rd.Next(0, n);
+            while(Block1 == Block2)
+            {
+                Block2 = rd.Next(0, n);
+            }
+            Block1 *= n;
+            Block2 *= n;
+            for (int i = 0; i < n*n; i++)
+            {
+                var k = Block2;
+                for (int j = Block1; j < Block1 + n; j++)
+                {
+                    var temp = map[j, i];
+                    map[j,i] = map[k, i];
+                    map[k,i] = temp;
+                    k++;
+                }
+            }
+        }
+
+        public void SwapBlocksInCol()
+        {
+            Random rd = new Random();
+            var Block1 = rd.Next(0, n);
+            var Block2 = rd.Next(0, n);
+            while (Block1 == Block2)
+            {
+                Block2 = rd.Next(0, n);
+            }
+            Block1 *= n;
+            Block2 *= n;
+            for (int i = 0; i < n * n; i++)
+            {
+                var k = Block2;
+                for (int j = Block1; j < Block1 + n; j++)
+                {
+                    var temp = map[i,j];
+                    map[i,j] = map[i,k];
+                    map[i,k] = temp;
+                    k++;
+                }
+            }
+        }
+        #endregion
+        public void HideCell()
+        {
+            int N = 41;
+            Random rd = new Random();
+            while (N > 0)
+            {
+                for (int i = 0; i < n * n; i++)
+                {
+                    for (int j = 0;j < n * n; j++)
+                    {
+                        if (!string.IsNullOrEmpty(btns[i,j].Text))
+                        {
+                            int a = rd.Next(0, 5);
+                            btns[i, j].Text = a == 0 ? "": btns[i, j].Text; 
+                            btns[i,j].Enabled = a == 0 ? true : false;
+                            if (a == 0)
+                            {
+                                N--;
+                            }
+                            if (N <=0)
+                            {
+                                break;
+                            }
+                        }
+                        if (btns[i, j].Text != "")
+                        {
+                            hide[i,j] = btns[i,j].Text;
+                        }
+                            
+                        else
+                        {
+                            hide[i, j] = "-1";
+                        }
+
+                        if (N <= 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            
+        } //lam lung lo mot vai vtri trong bang 
+        #endregion
+        #region Tính Giờ
+        int value_timer = 300;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            DateTime dt = new DateTime();
+            try
+            {
+                value_timer--;
+                label1.Text = dt.AddSeconds(value_timer).ToString("mm:ss");
+            }
+            catch
+            {
+                timer1.Stop();
+                MessageBox.Show("Hết giờ !!!", "Thông báo", MessageBoxButtons.OK);
+                for (int i = 0; i < n*n; i++)
+                {
+                    for (int j = 0; j < n*n; j++)
+                    {
+                        btns[i,j].Enabled = false;
+                        if (btns[i,j].Text == hide[i,j].ToString())
+                            btns[i,j].BackColor = Color.LightGray;
+                        else
+                            btns[i, j].BackColor = Color.White;
+                    }
+                }
+                
+            }
+            
+        }
+        #endregion
+    }
+}
